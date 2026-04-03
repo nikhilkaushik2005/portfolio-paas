8fcd4b2fa39be20f6a6b166aea88ae30530a3689
nikhil <s.nikhilkaushik8a@gmail.com>
2026-04-03 20:56:20 +0530

    Fixed data pipeline: generator now passes all JSON fields to Jinja

--- generator/generate.py
+++ generator/generate.py
@@ -1,19 +1,22 @@
 import json
 import os
-from jinja2 import Template 
+from jinja2 import Template
 
-# Read the data that will be passed in
+# 1. Read the JSON file sent from the frontend
 with open('student_data.json', 'r') as f:
     data = json.load(f)
 
-# Read our HTML skeleton
-with open('template.html', 'r') as f:
+# 2. Read your beautiful new HTML template
+with open('template.html', 'r', encoding='utf-8') as f:
     template = Template(f.read())
 
-# Inject the data into the HTML
-output = template.render(name=data['name'], bio=data['bio'], skills=data['skills'])
+# 3. THE MAGIC FIX: The double asterisk (**data) tells Python to 
+# unpack EVERY piece of data in the JSON and hand it to Jinja.
+rendered_html = template.render(**data)
 
-# Save the finished product
+# 4. Save the final HTML to the dist folder for AWS
 os.makedirs('dist', exist_ok=True)
-with open('dist/index.html', 'w') as f:
-    f.write(output)
+with open('dist/index.html', 'w', encoding='utf-8') as f:
+    f.write(rendered_html)
+
+print("Website successfully generated!")
